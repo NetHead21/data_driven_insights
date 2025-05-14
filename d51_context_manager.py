6a070ddeafbf787516282d622fca0cc4c95b2a2d
nethead21 <junivensaavedra@gmail.com>
2025-05-14 20:51:14 +0800

    Added temp_log

--- d51_context_manager.py
+++ d51_context_manager.py
@@ -1,7 +1,11 @@
+from contextlib import contextmanager
+
+
 with open("data.txt", "w") as f:
     f.write("Hello, world!")
 
 # The file is automatically closed even if an error occurs
+
 
 class FileLogger:
     def __init__(self, filename: str):
@@ -12,9 +16,23 @@ class FileLogger:
         return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.file.write(">> Logging session ended.\n")
+        if exc_type:
+            self.file.write(f"Error: {exc_val}\n")
         self.file.close()
 
 
 with FileLogger("log.txt") as log:
     log.write(">> Loging session started.\n")
+
+
+@contextmanager
+def temporary_log(file_name: str):
+    f = open(file_name, "a")
+    try:
+        yield f
+    finally:
+        f.close()
+
+
+with temporary_log("log.txt") as file:
+    file.write("This is safer and cleaner.\n")
